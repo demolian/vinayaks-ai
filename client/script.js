6c872c103064de9a1a456e834c45703e872332c3
demolian <121742586+demolian@users.noreply.github.com>
2023-02-16 16:56:23 +0530

    Updated code

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bot.svg'; //./public/bot.svg
+import bot from './assets/bot.svg'; 
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -29,7 +29,7 @@ function typeText(element, text) {
     } else {
       clearInterval(interval);
     }
-  }, 20)
+  }, 0) // 20
 }
 
 function generateUniqueId() {
@@ -100,132 +100,18 @@ const handleSubmit = async (e) => {
     const data = await responce.json();
     const parsedData = data.bot.trim();
     typeText(messageDiv, parsedData);
-   
-    
-   /* const speech = new SpeechSynthesisUtterance(parsedData);
-    speech.lang = 'en-US'; // automatic speaking
-    speechSynthesis.speak(speech); */
-   
-    
-    if (parsedData) {
-      // Request permission to show notifications
-      Notification.requestPermission().then(function(permission) {
-      
-        if (permission === "granted") {
-          // Show notification
-          var notification = new Notification(parsedData);
-          // Copy the notification content
-          let content = notification.title + (notification.options ? "\n" + notification.options : "");
-          copyNotificationContent(content);
-        }
-        
-        
 
-        /* if (permission === "granted") { old working
-          // Show notification
-          var notification = new Notification(parsedData);
-          // Copy the notification content
-          copyNotificationContent(notification.title+" "+notification.options );
-          //+ ": " + notification.options
-        } */
-      });
-    } 
-    
-    function copyNotificationContent(content) {
-      // Create a new textarea element to copy the notification content to
-      var textarea = document.createElement("textarea");
-    
-      // Set the value of the textarea to the notification content
-      textarea.value = content;
-      // Add the textarea to the document so it can be selected
-      document.body.appendChild(textarea);
-      // Select the textarea
-      textarea.select();
-      // Copy the notification content to the clipboard
-      document.execCommand("copy");
-      // Remove the textarea from the document
-      document.body.removeChild(textarea);
-      // Alert the user that the notification content has been copied
-     
-     // alert("Notification content copied to clipboard!");
-     if (!("Notification" in window)) {
-      console.log("This browser does not support notifications.");
-    } else if (Notification.permission === "granted") {
-     // var notification = new Notification("Notification content copied to clipboard!");
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-          var notification = new Notification("Done!");
-        }
-      });
-    }
-    }
-     
-    
-    /*
-    // Create copy button
-    const copyButton = document.createElement('button');
-    copyButton.innerHTML = "Copy";
-    copyButton.classList.add("copy-button");
-    copyButton.addEventListener('click', function() {
-      navigator.clipboard.writeText(parsedData);
-      alert("Text Copied: " + parsedData);
-    }); */
+    messageDiv.addEventListener("click", function(){
+  
     if (navigator.clipboard) {
       navigator.clipboard.writeText(parsedData);
-    //  alert("Text Copied: " + parsedData);
+     
     } else {
-    //  alert("Clipboard API is not supported in this browser");
+      alert("Clipboard API is not supported in this browser");
     } 
-    
-    
-   /* if (parsedData) {
-      // Request permission to show notifications
-Notification.requestPermission().then(function(permission) {
-  if (permission === "granted") {
-    // Show notification
-    var notification = new Notification("You can proceed now!");
+  }) 
   }
-});
-    } */
-    // Append copy button to the message div
-   // messageDiv.appendChild(copyButton);
-  }
-  
-  
-
-/*
-  // bot's chatstripe
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
-
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-
-  const messageDiv = document.getElementById(uniqueId);
-  
-  loader(messageDiv);
-
-  // fetch data from server -> Bot's responce
-
-  const responce = await fetch('http://localhost:3000', {
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
-
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
-
-  if(responce.ok) {
-    const data = await responce.json();
-    const parsedData = data.bot.trim();
-    typeText(messageDiv, parsedData);
-    
- }*/ else {
+ else {
     const err = await responce.text();
 
     messageDiv.innerHTML = "Something went wrong";
